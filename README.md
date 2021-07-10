16e39af854e61ed35c74136e630ac61dae6abd5b
Sudip Pain <sudip.pain@ericsson.com>
2021-07-10 22:35:18 +0530

    Add some comment in readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Creational-Pattern
+
+# 1. Factory Pattern
+# 2. Abstract-Factory Pattern
+# 3. Singleton Pattern
+# 4. Prototype Patten
+# 5. Builder Patter(Most of the time i used in our project by implementing an interface)
--- src/singelton/design/pattern/SingeltonClass.java
+++ src/singelton/design/pattern/SingeltonClass.java
@@ -2,15 +2,25 @@ package singelton.design.pattern;
 
 public class SingeltonClass {
 	
+	/*** Early Loading Case 1****/
 	
-	private static SingeltonClass singletonInstance = new SingeltonClass();
-	 
+//	private static SingeltonClass singletonInstance = new SingeltonClass();
+	private static SingeltonClass singletonInstance =null;
 	private SingeltonClass(){
 		 
 	 }
 
 	//Get the only object available
-	   public static SingeltonClass getInstance(){
+	/*** Early Loading Case 1****/
+//	   public static SingeltonClass getInstance(){
+//	      return singletonInstance;
+//	   }
+	 public static SingeltonClass getInstance(){
+		 
+		 /*** Late binding Case 2 ***/
+		 if(singletonInstance==null) {
+			 return new  SingeltonClass();
+		 }
 	      return singletonInstance;
 	   }
 	   
